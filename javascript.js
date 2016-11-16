606d8b3d229bac43b28d16b6c4a8fc03c9655614
francesco bottos <francesco bottos>
2016-11-16 20:33:28 +0100

    versione finale

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function showForm(){
+function mostraForm(){
     
     var form = document.getElementById("add_form");
     if(form.style.display=="block"){
@@ -9,28 +9,35 @@ function showForm(){
 
 };
 
-function setOrder(){
+function cambiaMax(){
+    if(document.getElementById("maxAmount").readOnly == true)
+        document.getElementById("maxAmount").readOnly = false;
+    else
+        document.getElementById("maxAmount").readOnly = true;
+};
+
+function setOrdine(){
     
     var maxAmount = document.getElementById("maxAmount").value;
-    namelist = document.getElementById("main_table").rows[0].cells;
-    qtàlist = document.getElementById("main_table").rows[1].cells;
+    nomi = document.getElementById("table").rows[0].cells;
+    quant = document.getElementById("table").rows[1].cells;
     var count = 0;
-    for(var i=0; i < namelist.length; i++){
-        if(namelist[i].innerHTML == document.getElementById("it_name").value){
-            var numqtà = parseInt(qtàlist[i].innerHTML,10);
+    for(var i=0; i < nomi.length; i++){
+        if(nomi[i].innerHTML == document.getElementById("it_name").value){
+            var numqtà = parseInt(quant[i].innerHTML,10);
             numqtà += +document.getElementById("it_qty").value;
-            qtàlist[i].innerHTML = numqtà;
+            quant[i].innerHTML = numqtà;
             count++;
             if(numqtà > maxAmount){
-                alert("too many items ordered");
-                qtàlist[i].innerHTML = maxAmount;
+                alert("Non puoi ordinare così tanti oggetti di un solo tipo");
+                quant[i].innerHTML = maxAmount;
             }
         }
     }
     if(count == 0){
-        var tmp_it = document.getElementById("it_list").insertCell(0);
+        var tmp_it = document.getElementById("itemx").insertCell(0);
         tmp_it.innerHTML = document.getElementById("it_name").value;
-        var tmp_qty = document.getElementById("qty_list").insertCell(0);
+        var tmp_qty = document.getElementById("qtyx").insertCell(0);
         if(Number(document.getElementById("it_qty").value)>maxAmount){
             tmp_qty.innerHTML = maxAmount;  
              alert("too many items ordered");
@@ -43,12 +50,3 @@ function setOrder(){
     
 };
 
-function changeMaxAm(){
-    
-    if(document.getElementById("maxAmount").readOnly == true)
-        document.getElementById("maxAmount").readOnly = false;
-    else
-        document.getElementById("maxAmount").readOnly = true;
-    
-    
-};
